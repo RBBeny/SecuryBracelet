d2672da338da7d749a9b503f4a2bebc54fe31895
RBBeny <72763492+RBBeny@users.noreply.github.com>
2022-08-17 11:47:00 -0500

    Add files via upload

---
+++ CodigoFuente_Security_Bracelet.ino
@@ -0,0 +1,297 @@
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Fonts/FreeSerif9pt7b.h>
+
+#include <ESP_Mail_Client.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+
+#define WIFI_SSID "BENY"
+#define WIFI_PASSWORD "123456789"
+
+#define SMTP_HOST "smtp.gmail.com"
+#define SMTP_PORT 465
+
+/* The sign in credentials */
+#define AUTHOR_EMAIL "benjaramirez2501@gmail.com"
+#define AUTHOR_PASSWORD "roxukrkysbysvubn"
+
+/* Recipient's email*/
+#define RECIPIENT_EMAIL "sgmd682@gmail.com"
+
+SMTPSession smtp;
+
+/* Callback function to get the Email sending status */
+void smtpCallback(SMTP_Status status);
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+TinyGPSPlus gps;
+SoftwareSerial SerialGPS(12, 14); 
+
+const char* ssid = "BENY";
+const char* password = "123456789";
+
+float Latitude , Longitude;
+int year , month , date, hour , minute , second;
+String DateString , TimeString , LatitudeString , LongitudeString;
+
+
+WiFiServer server(80);
+void setup()
+{
+  Serial.begin(115200);
+  SerialGPS.begin(9600);
+  Serial.println();
+  Serial.print("Connecting");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  server.begin();
+  Serial.println("Server started");
+  Serial.println(WiFi.localIP());
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+  Serial.println("SSD1306 allocation failed");
+  for(;;);
+  }
+  delay(2000);
+}
+
+void loop()
+{
+  while (SerialGPS.available() > 0)
+    if (gps.encode(SerialGPS.read()))
+    {
+      if (gps.location.isValid())
+      {
+        Latitude = gps.location.lat();
+        LatitudeString = String(Latitude , 6);
+        Longitude = gps.location.lng();
+        LongitudeString = String(Longitude , 6);
+      }
+
+      if (gps.date.isValid())
+      {
+        DateString = "";
+        date = gps.date.day();
+        month = gps.date.month();
+        year = gps.date.year();
+
+        if (date < 10)
+        DateString = '0';
+        DateString += String(date);
+
+        DateString += " / ";
+
+        if (month < 10)
+        DateString += '0';
+        DateString += String(month);
+        DateString += " / ";
+
+        if (year < 10)
+        DateString += '0';
+        DateString += String(year);
+      }
+
+      if (gps.time.isValid())
+      {
+        TimeString = "";
+        hour = gps.time.hour()- 5; //adjust UTC
+        minute = gps.time.minute();
+        second = gps.time.second();
+    
+        if (hour < 10)
+        TimeString = '0';
+        TimeString += String(hour);
+        TimeString += " : ";
+
+        if (minute < 10)
+        TimeString += '0';
+        TimeString += String(minute);
+        TimeString += " : ";
+
+        if (second < 10)
+        TimeString += '0';
+        TimeString += String(second);
+      }
+
+    }
+    
+  display.setFont(&FreeSerif9pt7b);
+  display.clearDisplay();
+  display.setTextSize(1);             
+  display.setTextColor(WHITE);        
+  display.setCursor(0,20);             
+  display.println("Hora:"+ TimeString );
+  display.setCursor(0,40);             
+  display.println("Lat:"+ LatitudeString);
+  display.setCursor(0,60);             
+  display.println("Lon:"+LongitudeString);
+  display.display();
+
+
+  
+  WiFiClient client = server.available();
+
+  
+  if (!client)
+  {
+    return;
+  }
+
+  //Response
+  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n <!DOCTYPE html> <html> <head> <title>Mi ubi</title> <style>";
+  s += "table, th, td {border: 1px solid blue;} </style> </head> <body> <h1  style=";
+  s += "font-size:300%;";
+  s += " ALIGN=CENTER>Mi ubicacion</h1>";
+  s += "<p ALIGN=CENTER style=""font-size:150%;""";
+  s += "> <b>Location Details</b></p> <table ALIGN=CENTER style=";
+  s += "width:50%";
+  s += "> <tr> <th>Latitude</th>";
+  s += "<td ALIGN=CENTER >";
+  s += LatitudeString;
+  s += "</td> </tr> <tr> <th>Longitude</th> <td ALIGN=CENTER >";
+  s += LongitudeString;
+  s += "</td> </tr> <tr>  <th>Fecha</th> <td ALIGN=CENTER >";
+  s += DateString;
+  s += "</td></tr> <tr> <th>Hora</th> <td ALIGN=CENTER >";
+  s += TimeString;
+  s += "</td>  </tr> </table> ";
+ 
+  
+  if (gps.location.isValid())
+  {
+    s += "<p align=center><a style=""color:RED;font-size:125%;"" href=""http://maps.google.com/maps?&z=15&mrt=yp&t=k&q=";
+    s += LatitudeString;
+    s += "+";
+    s += LongitudeString;
+    s += """ target=""_top"">Click here</a> to open the location in Google Maps.</p>";
+  }
+
+  s += "</body> </html> \n";
+
+  smtp.debug(1);
+
+  /* Set the callback function to get the sending results */
+  smtp.callback(smtpCallback);
+
+  /* Declare the session config data */
+  ESP_Mail_Session session;
+
+  /* Set the session config */
+  session.server.host_name = SMTP_HOST;
+  session.server.port = SMTP_PORT;
+  session.login.email = AUTHOR_EMAIL;
+  session.login.password = AUTHOR_PASSWORD;
+  session.login.user_domain = "mydomain.net";
+
+  /* Declare the message class */
+  SMTP_Message message;
+
+  /* Enable the chunked data transfer with pipelining for large message if server supported */
+  message.enable.chunking = true;
+
+  /* Set the message headers */
+  message.sender.name = "ESP Mail";
+  message.sender.email = AUTHOR_EMAIL;
+
+  message.subject = "Esta es mi ubicación actualmente";
+  message.addRecipient("Ubicación", RECIPIENT_EMAIL);
+
+  String ubi;
+  ubi += "<p align=center><a style=""color:RED;font-size:125%;"" href=""http://maps.google.com/maps?&z=15&mrt=yp&t=k&q=";
+  ubi += LatitudeString;
+  ubi += "+";
+  ubi += LongitudeString;
+  ubi += """ target=""_top"">Click here</a> to open the location in Google Maps.</p>";
+
+  /** Two alternative content versions are sending in this example e.g. plain text and html */
+  String htmlMsg = ubi;
+  message.html.content = htmlMsg.c_str();
+  message.html.charSet = "utf-8";
+  message.html.transfer_encoding = Content_Transfer_Encoding::enc_qp;
+
+  message.priority = esp_mail_smtp_priority::esp_mail_smtp_priority_normal;
+  message.response.notify = esp_mail_smtp_notify_success | esp_mail_smtp_notify_failure | esp_mail_smtp_notify_delay;
+
+  /* The attachment data item */
+  SMTP_Attachment att;
+
+  /** Set the attachment info e.g. 
+   * file name, MIME type, file path, file storage type,
+   * transfer encoding and content encoding
+  */
+  //att.descr.filename = "image.png";
+  //att.descr.mime = "image/png"; //binary data
+  //att.file.path = "/image.png";
+  //att.file.storage_type = esp_mail_file_storage_type_flash;
+  //att.descr.transfer_encoding = Content_Transfer_Encoding::enc_base64;
+
+  /* Add attachment to the message 
+  message.addAttachment(att);
+
+ att.descr.filename = "text_file.txt";
+  att.descr.mime = "text/plain";
+  att.file.path = "/text_file.txt"; message.resetAttachItem(att);
+  
+  att.file.storage_type = esp_mail_file_storage_type_flash;
+  att.descr.transfer_encoding = Content_Transfer_Encoding::enc_base64;
+*/
+  /* Add attachment to the message */
+    //message.addAttachment(att);
+    if (!smtp.connect(&session))
+        return;
+
+ 
+
+  /* Start sending the Email and close the session */
+  if (!MailClient.sendMail(&smtp, &message, true))
+    Serial.println("Error sending Email, " + smtp.errorReason());
+
+  
+  client.print(s);
+  delay(10000);
+
+}
+
+
+void smtpCallback(SMTP_Status status){
+  /* Print the current status */
+  Serial.println(status.info());
+
+  /* Print the sending result */
+  if (status.success()){
+    Serial.println("----------------");
+    ESP_MAIL_PRINTF("Message sent success: %d\n", status.completedCount());
+    ESP_MAIL_PRINTF("Message sent failled: %d\n", status.failedCount());
+    Serial.println("----------------\n");
+    struct tm dt;
+
+    for (size_t i = 0; i < smtp.sendingResult.size(); i++){
+      /* Get the result item */
+      SMTP_Result result = smtp.sendingResult.getItem(i);
+      time_t ts = (time_t)result.timestamp;
+      localtime_r(&ts, &dt);
+
+      ESP_MAIL_PRINTF("Message No: %d\n", i + 1);
+      ESP_MAIL_PRINTF("Status: %s\n", result.completed ? "success" : "failed");
+      ESP_MAIL_PRINTF("Date/Time: %d/%d/%d %d:%d:%d\n", dt.tm_year + 1900, dt.tm_mon + 1, dt.tm_mday, dt.tm_hour, dt.tm_min, dt.tm_sec);
+      ESP_MAIL_PRINTF("Recipient: %s\n", result.recipients);
+      ESP_MAIL_PRINTF("Subject: %s\n", result.subject);
+    }
+    Serial.println("----------------\n");
+  }
+}
